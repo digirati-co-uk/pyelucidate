76b76f3b48ecbdfa4687cdf22ab0b65a3d468b32
Matt McGrattan <matt.mcgrattan@digirati.com>
2019-01-11 15:45:21 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ history = open("HISTORY.rst").read().replace(".. :changelog:", "")
 
 setup(
     name="pyelucidate",
-    version="0.2.2",
+    version="0.3.0",
     description="Open Source Python Tools for the Elucidate Annotation Server.",
     long_description=readme + "\n\n" + doclink + "\n\n" + history,
     author="Matt McGrattan",
